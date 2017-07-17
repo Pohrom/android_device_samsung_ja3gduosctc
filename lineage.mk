d67ed3b142baadbcce4d3a7af91757b341deedb1
上铺小哥 <Spxg0923@gmail.com>
2017-07-17 15:14:24 +0800

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,15 +1,16 @@
-$(call inherit-product, device/samsung/ja3gchnduos/full_ja3gchnduos.mk)
+$(call inherit-product, device/samsung/ja3gduosctc/full_ja3gduosctc.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_ja3gchnduos
-PRODUCT_DEVICE := ja3gchnduos
+
+PRODUCT_NAME := lineage_ja3gduosctc
+PRODUCT_DEVICE := ja3gduosctc
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_MODEL=GT-I9502 \
-    PRODUCT_NAME=ja3gchnduos \
-    PRODUCT_DEVICE=ja3gchnduos \
-    TARGET_DEVICE=ja3gchnduos \
-    BUILD_FINGERPRINT="samsung/ja3gchnduoszn/ja3gchnduos:5.0.1/LRX22C/I9502ZNUJOH2:user/release-keys" \
-    PRIVATE_BUILD_DESC="a3gchnduoszn-user 5.0.1 LRX22C I9502ZNUJOH2 release-keys"
+    PRODUCT_MODEL=SCH-I959 \
+    PRODUCT_NAME=ja3gduosctc \
+    PRODUCT_DEVICE=ja3gduosctc \
+    TARGET_DEVICE=ja3gduosctc \
+    BUILD_FINGERPRINT="samsung/ja3gduosctc/ja3gduosctc:5.0.1/LRX22C/I959KEUJOH4:user/release-keys" \
+    PRIVATE_BUILD_DESC="ja3gduosctc-user 5.0.1 LRX22C I959KEUJOH4 release-keys"
